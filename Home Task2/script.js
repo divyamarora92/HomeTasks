ed34df9ff79f6d4829a9aeb60e2130d22521f05e
Divyam Arora <divyamarora92@gmail.com>
2022-04-05 17:14:44 +0530

    New Files Added

---
+++ Home Task2/script.js
@@ -0,0 +1,41 @@
+let videos=document.getElementById("videos");
+$(document).ready(function(){
+    var API_KEY="AIzaSyAoIIJI4tST-ecOzzg_q_mm89vt-e9cTdQ";
+    var video='';
+    var desc=""
+    $("#form").submit(function(event){
+        event.preventDefault()
+        var search=$("#search").val()
+        // alert("form is submit");
+        videoSearch(API_KEY,search,15)
+    })
+    function videoSearch(key,search,maxResults){
+        $.get("https://www.googleapis.com/youtube/v3/search?key="+key
+        +"&type=video&part=snippet&maxResults="+maxResults+"&q="+search,function(data){
+            console.log(data)
+            data.items.forEach(item=>{
+                desc=`${item.snippet.description}`;
+                
+                video=`
+               
+                <iframe id="player" type="text/html" width="640" height="390"src="http://www.youtube.com/embed/${item.id.videoId}"
+  frameborder="0"></iframe>
+                `
+                let addVideo=document.createElement('div');
+                let addDesc=document.createElement('p');
+                addVideo.className="items";
+                addDesc.className="desc";
+                addVideo.innerHTML=video;
+                addDesc.innerHTML=desc;
+                videos.appendChild(addVideo);
+                videos.appendChild(addDesc);
+                // $("#videos").append(video)
+                // $("#desc").append(desc)
+                
+                
+            })
+        })
+    }
+})
+
+//  <iframe width="420" height="315" src="https://www.youtube.com.embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
